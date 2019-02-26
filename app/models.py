14704e5f65084acc47d66f07d01f5909eb3f26bd
Thomas Bigger <thomas@performanceactive.com>
2019-02-26 16:44:14 +0000

    see what this looks like, continue ith the cmparisons of models saying we now have the 3 datasets

--- app/models.py
+++ app/models.py
@@ -21,7 +21,8 @@ class LogMessage(models.Model):
 
 
 # constants
-base_folder = 'app/trained_models'
+data_folder = 'app/data'
+model_folder = 'app/trained_models'
 popularity = 'popularity'
 cosine = 'cosine'
 pearson = 'pearson'
@@ -39,7 +40,7 @@ class ItemRecommender:
             pd.reset_option('display.max_rows')
 
         s = time.time()
-        transactions = pd.read_csv('app/data/trx_data.csv')
+        transactions = pd.read_csv(data_folder + '/trx_data.csv')
 
         transactions['products'] = transactions['products'].apply(
             lambda x: [int(i) for i in x.split('|')])
@@ -138,12 +139,14 @@ class ItemRecommender:
         9               10      84.0
         """
 
-        data_group_by = dataset_dropped_var.groupby(['customerId', 'products'])
+        data_group_by = dataset_dropped_var\
+            .groupby(['customerId', 'products'])
         """
             pandas.core.groupby.generic.DataFrameGroupBy object at 0x125b5b940
         """
 
-        count_aggregate = data_group_by.agg({'products': 'count'})
+        count_aggregate = data_group_by\
+            .agg({'products': 'count'})
         """
         shape (133585, 1)
                             products
@@ -161,10 +164,13 @@ class ItemRecommender:
                    198.0            1
         """
 
-        count_aggregate_renamed_cols = count_aggregate.rename(
-            columns={'products': 'purchase_count'}).reset_index().rename(columns={'products': 'productId'})
-        count_aggregate_renamed_cols['productId'] = count_aggregate_renamed_cols['productId'].astype(
-            np.int64)
+        count_aggregate_renamed_cols = count_aggregate\
+            .rename(columns={'products': 'purchase_count'})\
+            .reset_index()\
+            .rename(columns={'products': 'productId'})
+
+        count_aggregate_renamed_cols['productId'] =\
+            count_aggregate_renamed_cols['productId'].astype(np.int64)
         """
         shape (133585, 3)
                 customerId  productId  purchase_count
@@ -184,32 +190,60 @@ class ItemRecommender:
         data = count_aggregate_renamed_cols
         # -----------------------------------------------------------------------------------------
 
-        def create_data_dummy(data):
-            data_dummy = data.copy()
-            data_dummy['purchase_dummy'] = 1
-            return data_dummy
+        # Dummy for marking whether a customer bought that item or not.
+        # If one buys an item, then purchase_dummy are marked as 1
+        data_dummy = data.copy()
+        data_dummy['purchase_dummy'] = 1
 
-        data_dummy = create_data_dummy(data)
+        # Normalizing the purchase count, say by each user, would not work because customers may have different buying frequency don’t have the same taste.
+        # However, we can normalize items by purchase frequency across all users.
+        # we will normalize their purchase history, from 0–1 (with 1 being the most number of purchase for an item and 0 being 0 purchase count for that item
 
-        def normalize_data(data):
-            df_matrix = pd.pivot_table(
-                data, values='purchase_count', index='customerId', columns='productId')
-            df_matrix_norm = (df_matrix-df_matrix.min()) / \
-                (df_matrix.max()-df_matrix.min())
-            d = df_matrix_norm.reset_index()
-            d.index.names = ['scaled_purchase_freq']
-            return pd.melt(d, id_vars=['customerId'], value_name='scaled_purchase_freq').dropna()
+        df_matrix = pd.pivot_table(
+            data, values='purchase_count', index='customerId', columns='productId')
+        df_matrix_norm = (df_matrix-df_matrix.min()) / \
+            (df_matrix.max()-df_matrix.min())
+        d = df_matrix_norm.reset_index()
+        d.index.names = ['scaled_purchase_freq']
+        data_norm = pd.melt(
+            d, id_vars=['customerId'], value_name='scaled_purchase_freq').dropna()
+        """
+        shape (133585, 3)
+                customerId productId  scaled_purchase_freq
+        9                 9         0              0.133333
+        25               25         0              0.133333
+        32               33         0              0.133333
+        35               36         0              0.133333
+        43               44         0              0.133333
+        55               56         0              0.133333
+        59               60         0              0.000000
+        71               72         0              0.333333
+        75               76         0              0.133333
+        90               91         0              0.000000
+        97               98         0              0.333333
+        99              100         0              0.133333
+        """
+        # -----------------------------------------------------------------------------------------
 
-        data_norm = normalize_data(data)
+        # Split train and test set
+        # Typically, we use a larger portion of the data for training and a smaller portion for testing.
+        # We use 80:20 ratio for our train-test set size.
+        # Our training portion will be used to develop a predictive model, while the other to evaluate the model’s performance.
 
         def split_data(data):
+            # split ratio is 0.2
             train, test = train_test_split(data, test_size=split_ratio)
             train_data = tc.SFrame(train)
             test_data = tc.SFrame(test)
             return train_data, test_data
 
+        train_data, test_data = split_data(data)
         train_data_dummy, test_data_dummy = split_data(data_dummy)
         train_data_norm, test_data_norm = split_data(data_norm)
+
+        # Now that we have three datasets with purchase counts, purchase dummy, and scaled purchase counts
+
+        # -----------------------------------------------------------------------------------------
 
         def model(train_data, name, user_id, item_id, target, users_to_recommend, n_rec, n_display):
             if name == popularity:
@@ -229,7 +263,7 @@ class ItemRecommender:
                                                               item_id=item_id,
                                                               target=target,
                                                               similarity_type=pearson)
-            model.save(base_folder + '/' + name)
+            model.save(model_folder + '/' + name)
             return model
 
         # variables to define field names
@@ -240,10 +274,15 @@ class ItemRecommender:
         n_rec = 10  # number of items to recommend
         n_display = 30
 
-        # The popularity model takes the most popular items for recommendation. These items are products with the highest number of sells across customers.
+        # The popularity model takes the most popular items for recommendation.
+        # These items are products with the highest number of sells across customers.
         target = 'purchase_count'
         popularity_model = model(train_data_dummy, popularity, user_id,
                                  item_id, target, users_to_recommend, n_rec, n_display)
+
+        # ------ todo see what this looks like, continue ith the cmparisons of models saying we now have the 3 datasets
+        print(popularity_model)
+        return ''
 
         # In collaborative filtering, we would recommend items based on how similar users purchase items.
         # For instance, if customer 1 and customer 2 bought similar items, e.g. 1 bought X, Y, Z and 2 bought X, Y, we would recommend an item Z to customer 2.
@@ -258,7 +297,7 @@ class ItemRecommender:
         print("Execution time:", round((time.time()-s)/60, 2), "minutes")
 
     def recommend(self):
-        popularity_model = tc.load_model(base_folder + '/' + popularity)
+        popularity_model = tc.load_model(model_folder + '/' + popularity)
 
         users_to_recommend = list(1)
         n_rec = 10
