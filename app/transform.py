4d4eace9edcba95324e2524887c7502547e431d4
Thomas Bigger <thomas@performanceactive.com>
2019-03-01 13:13:33 +0000

    splitting the data into rows of 500 for insert to see if it works with the database insert better with liquibase

--- app/transform.py
+++ app/transform.py
@@ -17,20 +17,43 @@ class DataTransform:
         userData['activated'] = True
         userData['password_hash'] = '$2a$10$HdQfU7GJ8xTh7V23joLEe.qcySJz./z6bO0NKfJLNwbkfRYG9mXbu'
         userData['lang_key'] = 'en'
+        userData['created_date'] = '2019-03-01 11:16:45.079'
         userData['created_by'] = 'system'
         userData['last_modified_by'] = 'system'
 
         customerData = customerIds.join(userData)
-        customerData.columns = ['id', 'first_name', 'last_name', 'email', 'login',
-                                'activated', 'password_hash', 'lang_key', 'created_by', 'last_modified_by']
-        customerData.set_index('id')
-        customerData.to_csv(
-            'app/data/user_seed/user_seed_data.csv', sep=';', index=False)
+        columnNames = ['id', 'first_name', 'last_name', 'email', 'login',
+                       'activated', 'password_hash', 'lang_key', 'created_date', 'created_by', 'last_modified_by']
+        customerData.columns = columnNames
+        
+        divisor = 500
 
-        userAuthorities = customerIds.copy()
-        userAuthorities['authority_name'] = 'ROLE_USER'
-        userAuthorities.columns = ['user_id', 'authority_name']
-        userAuthorities.set_index('user_id')
-        userAuthorities.to_csv(
-            'app/data/user_seed/authority_seed_data.csv', sep=';', index=False)
+        count = 0
+        collectDf = pd.DataFrame(columns=columnNames)
+        try:
+           for row in range(0, customerData.shape[0]):
+            thisRow = customerData.values[row]
+            thisDataframe = pd.DataFrame([thisRow], columns=columnNames)
+            collectDf = collectDf.append(thisDataframe)
+            if (row != 0 and row % divisor == 0):
+                collectDf.to_csv('app/data/user_seed/user_seed_data' +
+                                 str(row) + '.csv', sep=';', index=False)
+                collectDf = pd.DataFrame(columns=columnNames)
+                count = row
+        except IndexError: 
+            pass
+        
+        count = count + 1
+        if count < customerData.shape[0]:
+            collectDf = customerData.iloc[count:customerData.shape[0]]
+            collectDf.to_csv('app/data/user_seed/user_seed_data' +
+                                 str(customerData.shape[0]) + '.csv', sep=';', index=False)
+            
+        
 
+        # userAuthorities = customerIds.copy()
+        # userAuthorities['authority_name'] = 'ROLE_USER'
+        # userAuthorities.columns = ['user_id', 'authority_name']
+        # userAuthorities.set_index('user_id')
+        # userAuthorities.to_csv(
+        #     'app/data/user_seed/authority_seed_data.csv', sep=';', index=False)
